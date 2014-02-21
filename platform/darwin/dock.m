c4e1a1a7389208220834434fd94ed8be9098ef15
Andraz Vrhovec <andraz.vrhovec@xlab.si>
2014-02-21 13:22:57 +0100

    OSX tray icon

--- platform/darwin/dock.m
+++ platform/darwin/dock.m
@@ -1,82 +1,63 @@
+
+// #cgo CFLAGS: -DDARWIN -x objective-c
+// #cgo LDFLAGS: -framework Cocoa
 #import <Cocoa/Cocoa.h>
 
-static volatile NSString *strStaticUrl;
-static volatile NSURL *staticUrl;
+NSMenu* appMenu;
 
-@interface AppDelegate: NSObject <NSApplicationDelegate>
-- (NSMenu *)applicationDockMenu:(NSApplication *)sender;
+extern void tray_callback(int itemId);
+
+@interface ManageHandler : NSObject
++ (IBAction)manage:(id)sender;
 @end
 
-@implementation AppDelegate
-NSString *m_title;
-NSMenu *m_menu;
-- (id)init:(NSString *)title {
-    if ((self = [super init])) {
-        m_title = title;
-    }
-    return self;
-}
-
-- (NSMenu *)applicationDockMenu:(NSApplication *)sender {
-    if (m_menu == nil) {
-        m_menu = [[[NSMenu alloc] init] retain];
-
-        id titleMenuItem = [[[NSMenuItem alloc] initWithTitle:m_title action:@selector(terminate:) keyEquivalent:@"q"] autorelease];
-
-        [m_menu addItem:titleMenuItem];
-        [m_menu addItem:[NSMenuItem separatorItem]];
-        NSMenuItem *openMenuItem = [[[NSMenuItem alloc] initWithTitle:@"Open in Browser" action:@selector(openUrl) keyEquivalent:@""] autorelease];
-        [m_menu addItem:openMenuItem];
-        NSMenuItem *copyUrlMenuItem = [[[NSMenuItem alloc] initWithTitle:@"Copy link to Clipboard" action:@selector(copyUrl) keyEquivalent:@""] autorelease];
-        [m_menu addItem:copyUrlMenuItem];
-
-        // OSX will automatically add the Quit option
-
-    }
-    return m_menu;
-}
-- (void)copyUrl {
-    if (strStaticUrl) {
-        NSPasteboard *pasteboard = [NSPasteboard generalPasteboard];
-        [pasteboard clearContents];
-        NSArray *copiedObjects = [NSArray arrayWithObject:strStaticUrl];
-        [pasteboard writeObjects:copiedObjects];
-    }
-}
-- (void)openUrl {
-    if (staticUrl) {
-        [[NSWorkspace sharedWorkspace] openURL:(NSURL*)staticUrl];
-    }
+@implementation ManageHandler
++ (IBAction)manage:(id)sender {
+    tray_callback([[sender representedObject] intValue]);
 }
 @end
 
+void add_menu_item(int itemId, const char *title, int disabled) {
+    NSString* manageTitle = [NSString stringWithCString:title encoding:NSASCIIStringEncoding];
+    NSMenuItem* menuItem = [[[NSMenuItem alloc] initWithTitle:manageTitle
+                                action:@selector(manage:) keyEquivalent:@""]
+                                autorelease];
 
-void native_loop(const char *title, unsigned char *imageDataBytes, unsigned int imageDataLen) {
-    [NSAutoreleasePool new];
-    [NSApplication sharedApplication];
-    [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+    [menuItem setRepresentedObject:[NSNumber numberWithInt:itemId]];
+    [menuItem setTarget:[ManageHandler class]];
+    [menuItem setEnabled: !(BOOL)disabled];
+    [appMenu addItem:menuItem];
+}
 
-    NSData *imageData = [[[NSData alloc] initWithBytes:imageDataBytes length:imageDataLen] autorelease];
-    NSImage *icon = [[[NSImage alloc] initWithData:imageData] autorelease];
+void add_separator_item() {
+    [appMenu addItem:[NSMenuItem separatorItem]];
+}
 
-    [NSApp setApplicationIconImage:icon];
-
-    AppDelegate *delegate = [[[AppDelegate alloc] init:[NSString stringWithCString:title encoding:NSASCIIStringEncoding]] autorelease];
-    [NSApp setDelegate:delegate];
-
-    [NSApp activateIgnoringOtherApps:YES];
+void native_loop() {
     [NSApp run];
-
-    // I don't think this is ever reached, but for completeness...
-    [staticUrl release];
-    [strStaticUrl release];
 }
 
-void set_url(const char *url) {
-    // Hoping these assignments are atomic...
-    // Using alloc to prevent the framework from trying to autorelease these.
-
-    strStaticUrl = [[NSString alloc] initWithBytes:url length:strlen(url) encoding:NSASCIIStringEncoding];
-    staticUrl = [[NSURL alloc] initWithString:(NSString*)strStaticUrl];
+void exit_loop() {
+    [NSApp stop:nil];
 }
 
+int init(const char *title, unsigned char imageDataBytes[], unsigned int imageDataLen) {
+
+    [NSAutoreleasePool new];
+
+    [NSApplication sharedApplication];
+    [NSApp setActivationPolicy:NSApplicationActivationPolicyProhibited];
+
+    appMenu = [[NSMenu new] autorelease];
+    [appMenu setAutoenablesItems: NO];
+
+    NSData *iconData = [NSData dataWithBytes:imageDataBytes length:imageDataLen];
+    NSImage *icon = [[NSImage alloc] initWithData:iconData];
+
+    NSStatusItem* statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength] retain];
+    [statusItem setMenu:appMenu];
+    [statusItem setImage:icon];
+    [statusItem setHighlightMode:NO];
+
+    return 0;
+}
